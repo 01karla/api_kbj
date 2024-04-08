03a38b366684386800bb0fcaa56f58e397c9aaa2
01karla <120067728+01karla@users.noreply.github.com>
2024-04-07 21:48:57 -0600

    bd2.1

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require ('mysql');
 
 const connection = mysql.createConnection({
-host:'localhost',
+host:'databases.000webhost.com',
 user: 'id21144240_kbj_api',
 password:'Karla099#',
 database:'id21144240_kbj_api'
